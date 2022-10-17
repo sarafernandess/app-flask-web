b3363933a1f743e83271ac0e560bfd94431ddff1
sarafernandess <sarafernandes530@gmail.com>
2022-10-17 09:18:51 -0300

    index-blueprint

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from index import index_blueprint
 
 def main():
     application = Flask(__name__)
 
     application.config.from_object("config.DevelopmentConfig")
+
+    application.register_blueprint(index_blueprint)
 
     application.run(host="0.0.0.0", port=5000)
 
