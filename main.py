33ae922663ff099873bf1a289bfeb18861e6c314
sarafernandess <sarafernandes530@gmail.com>
2022-10-17 11:18:17 -0300

    crawler-py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from index import index_blueprint
+
+application = Flask(__name__)
+application.config.from_object("config.DevelopmentConfig")
 
 database = SQLAlchemy()
+database.init_app(application)
+
+
+from index import index_blueprint
+from character import character_blueprint
 
 def main():
-    application = Flask(__name__)
+    # from util.crawler import get_data_from_rick_and_morty_apis
 
-    application.config.from_object("config.DevelopmentConfig")
-
-    database.init_app(application)
+    if application.config["TESTING"]:
+        with application.app_context():
+            database.drop_all()
+            database.create_all()
+            # get_data_from_rick_and_morty_apis(database)
 
     application.register_blueprint(index_blueprint)
+    application.register_blueprint(character_blueprint)
 
     application.run(host="0.0.0.0", port=5000)
+
 
 if __name__ == "__main__":
     main()
